f5a225640797afefda4aa5e7327ae383525b3bfa
Sucharek233 <noreply@sucharek.233>
2026-08-27 12:55:08 +0200

    add apps activity

---
+++ src/router/activities/apps.js
@@ -0,0 +1,37 @@
+import MailboxState from "../../constants/mailboxStates";
+
+export default class Apps {
+    static type = "apps";
+    static needsLua = true;
+
+    constructor(mailbox) {
+        this.mailbox = mailbox;
+    }
+
+    async handle(message) {
+        return await this.run(message.args);
+    }
+
+    async run(args) {
+        try {
+            const result = await this.mailbox.request(
+                Apps.type,
+                args
+            );
+
+            const appState = result.appState;
+            return {
+                type: Apps.type,
+                res: result.res,
+                state: appState
+            }
+        } catch (e) {
+            return {
+                type: Apps.type,
+                state: MailboxState.ERROR,
+                msg: e.message,
+                stack: e.stack
+            }
+        }
+    }
+}
