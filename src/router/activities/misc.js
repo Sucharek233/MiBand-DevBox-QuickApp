75b496b54cd634ab953e4fda944438952fc386ff
Sucharek233 <31042508+Sucharek233@users.noreply.github.com>
2026-09-11 18:56:20 +0200

    add argsValidator to misc

--- src/router/activities/misc.js
+++ src/router/activities/misc.js
@@ -1,4 +1,14 @@
 import MailboxState from "../../constants/mailboxStates";
+import ArgsValidator from "../../helpers/argsValidator";
+
+const handlers = {
+    gc: {
+        run: function(self, _) {
+            global.runGC();
+            return self.getResult("Ok");
+        }
+    }
+};
 
 export default class Misc {
     static type = "misc";
@@ -9,32 +19,46 @@ export default class Misc {
     }
 
     async handle(message) {
-        return await this.run(message.args);
-    }
+        const args = message.args || {};
+        const type = args.type;
 
-    // 
-    getResult(msg) {
-        return {
-            type: Misc.type,
-            state: MailboxState.DONE,
-            res: msg
+        const handler = handlers[type];
+
+        if (!handler) {
+            return {
+                type: Misc.type,
+                state: MailboxState.ERROR,
+                msg: "Unknown type"
+            };
         }
-    }
 
-    async run(args) {
+        const validation = ArgsValidator.validate(args, handler);
+
+        if (!validation.valid) {
+            return {
+                type: Misc.type,
+                state: MailboxState.ERROR,
+                msg: validation.error
+            };
+        }
+
         try {
-            const type = args.type;
-            if (type == "gc") {
-                global.runGC();
-                return this.getResult();
-            }
+            return await handler.run(this, args);
         } catch (e) {
             return {
                 type: Misc.type,
                 state: MailboxState.ERROR,
                 msg: e.message,
                 stack: e.stack
-            }
+            };
         }
+    }
+
+    getResult(msg) {
+        return {
+            type: Misc.type,
+            state: MailboxState.DONE,
+            res: msg
+        };
     }
 }
